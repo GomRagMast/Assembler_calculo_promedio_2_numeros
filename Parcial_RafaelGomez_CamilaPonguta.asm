bffb4acf2529bcf1a7eec69c3801d9f0ca71a8b5
Rafael Gomez <gomezrafaelantonio@gmail.com>
2016-10-14 02:07:43 -0500

    Imprime colores rojo mayor o igual a 1, azul menor a 1 - error en el calculo de promedio

--- Parcial_RafaelGomez_CamilaPonguta.asm
+++ Parcial_RafaelGomez_CamilaPonguta.asm
@@ -11,12 +11,16 @@ inicio:
     int 21h            
     ;
     call leer  
-    sub al, 30h 
+    sub al, 30h  
+    mul mult
     mov nota1, al
     call leer
     call leer   
     sub al, 30h
     mov nota11, al
+    mov al, nota1
+    add al, nota11
+    mov nota1, al
     
     ;salto de linea
     mov al, 0ah
@@ -26,31 +30,108 @@ inicio:
     mov ah, 0eh    
     int 10h
     
-    ;nota 2 
-    mov dx, offset msg1
+    ;nota 1
+    mov dx, offset msg2
     mov ah, 09h        
-    int 21h
+    int 21h            
     ;
-    call leer 
+    call leer  
     sub al, 30h  
+    mul mult
     mov nota2, al
     call leer
-    call leer  
+    call leer   
     sub al, 30h
-    mov nota21, al 
+    mov nota21, al
+    mov al, nota2
+    add al, nota21
+    mov nota2, al 
     
-    ;call calculo
+    call calculo  
+    
+    ;salto de linea
+    mov al, 0ah
+    mov ah, 0eh
+    int 10h
+    mov al, 0Dh
+    mov ah, 0eh    
+    int 10h
+    
+    mov dx, offset msg3
+    mov ah, 09h        
+    int 21h
+    
+    mov al, notaa
+    cmp al, 1
+    jl et2
+    
+et1:
+    mov bl, 04h
+    jmp fin
+et2:
+    mov bl, 09h
+    jmp fin
+fin:    
+    mov dl, 17
+    mov bh, 0           
+    mov ah, 2           
+    int 10h  
+    
+    mov al, notaa 
+    mov cx, 1          
+    add al, 30h
+    mov ah, 09h
+    int 10h      
+     
+    add dl, 1
+    mov bh, 0           
+    mov ah, 2           
+    int 10h
+    
+    mov al, ','        
+    mov cx, 1          
+    mov ah, 09h
+    int 10h  
+    
+    add dl, 1
+    mov bh, 0           
+    mov ah, 2           
+    int 10h  
+    
+    mov al, notab
+    mov cx, 1
+    add al, 30h
+    mov ah, 09h
+    int 10h      
+    
 ret
 
 calculo:
+    mov al, nota1
+    add al, nota2
+    mov aux, al
+    xor ax,ax
+    mov al, divi
+    mov bl, al
+    mov al, aux
+    div bl     
+    mov notaa, al
+    mov aux, bl  
     
-
-;    multiplicacion: 
-;        mov ax,aux 
-;        mul 10
-;        mov resu1, ax
-;        mov resu2, dx
-;    ret
+    xor ax,ax
+    mov al, mult
+    mov bl, al
+    mov al, notaa
+    div bl
+    mov notaa, al 
+                  
+    xor ax,ax
+    mov al, divi
+    mov bl, al
+    mov al, aux
+    div bl
+    mov notab, al                                    
+ret 
 
 leer:
     mov ah,0
@@ -65,7 +146,9 @@ notaA db 0
 nota2 db 0
 nota21 db 0
 notaB db 0 
-aux dw 0
+aux db 0
+mult db 10                 
+divi db 2
 
 msg1 db "Ingrese la nota 1 (n,n): $"
 msg2 db "Ingrese la nota 2 (n,n): $"
